12a9af4660b410980f2810ee5ffee437de9cca28
Stefan Kürzeder <info@stivik.me>
2023-03-02 18:16:16 +0100

    Removed unused json tags

--- prompt/types.go
+++ prompt/types.go
@@ -2,7 +2,7 @@ package prompt
 
 // promptTemplate represents the json template for the prompt (see templates/*.json)
 type promptTemplate struct {
-	Label    string `json:"label"`
-	Active   string `json:"active"`
-	Inactive string `json:"inactive"`
+	Label    string
+	Active   string
+	Inactive string
 }
