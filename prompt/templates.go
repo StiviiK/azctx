8a1becfb5db67d0a11a67aa729f2bb295da8b25c
Stefan Kürzeder <info@stivik.me>
2023-03-02 18:12:35 +0100

    Renamed promptJsonTemplate to promptTemplate

--- prompt/templates.go
+++ prompt/templates.go
@@ -1,12 +1,12 @@
 package prompt
 
 var (
-	template_Long = promptJsonTemplate{
+	template_Long = promptTemplate{
 		Label:    "{{ repeat 4 \" \" }}{{ repeat %[1]d \" \" | print \"Name\" | trunc %[1]d }} {{ repeat 38 \" \" | print \"SubscriptionId\" | trunc 38 }} {{ repeat %[2]d \" \" | print \"Tenant\" | trunc %[2]d }}",
 		Active:   "▸ {{ repeat %[1]d \" \" | print .Name | trunc %[1]d | green | %[3]s }} | {{ repeat 36 \" \" | print .Id | trunc 36 | cyan | %[3]s }} | {{ repeat %[2]d \" \" | print \")\" | print .Tenant | print \" (\" | print .TenantName | trunc %[2]d | faint | %[3]s }}",
 		Inactive: "{{ repeat 2 \" \" }}{{ repeat %[1]d \" \" | print .Name | trunc %[1]d | green | %[3]s }} | {{ repeat 36 \" \" | print .Id | trunc 36 | cyan | %[3]s }} | {{ repeat %[2]d \" \" | print \")\" | print .Tenant | print \" (\" | print .TenantName | trunc %[2]d | faint | %[3]s }}",
 	}
-	template_Short = promptJsonTemplate{
+	template_Short = promptTemplate{
 		Label:    "{{ repeat 4 \" \" }}{{ repeat %[1]d \" \" | print \"Name\" | trunc %[1]d }} {{ repeat %[2]d \" \" | print \"Tenant\" | trunc %[2]d }}",
 		Active:   "▸ {{ repeat %[1]d \" \" | print .Name | trunc %[1]d | green | %[3]s }} | {{ repeat %[2]d \" \" | print .TenantName | trunc %[2]d | cyan | %[3]s }}",
 		Inactive: "{{ repeat 2 \" \" }}{{ repeat %[1]d \" \" | print .Name | trunc %[1]d | green | %[3]s }} | {{ repeat %[2]d \" \" | print .TenantName | trunc %[2]d | cyan | %[3]s }}",
@@ -14,7 +14,7 @@ var (
 )
 
 // templateName returns the template to use
-func loadTemplate() promptJsonTemplate {
+func loadTemplate() promptTemplate {
 	if ShortPrompt {
 		return template_Short
 	}
--- prompt/types.go
+++ prompt/types.go
@@ -1,7 +1,7 @@
 package prompt
 
-// promptJsonTemplate represents the json template for the prompt (see templates/*.json)
-type promptJsonTemplate struct {
+// promptTemplate represents the json template for the prompt (see templates/*.json)
+type promptTemplate struct {
 	Label    string `json:"label"`
 	Active   string `json:"active"`
 	Inactive string `json:"inactive"`
